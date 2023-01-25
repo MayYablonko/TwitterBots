d656286b2b1b9ace0a7f92dcf9280e83cc3107e1
MayYablonko <may.yab1999@gmail.com>
2023-01-25 19:35:07 +0200

    commits

--- RoyalCats/RoyalCatsBot.py
+++ RoyalCats/RoyalCatsBot.py
@@ -1,6 +1,3 @@
-if __name__ == "__main__":
-    print("hi")
-
 
 import tweepy
 import json
@@ -8,7 +5,26 @@ import os.path
 import time
 from datetime import datetime
 import sys
+import requests
+
+from KEYSnotforpublic import royalcatskeys as rck
 
 
+####################################
+
+def api():
+    auth = tweepy.OAuthHandler(rck.api_key, rck.api_secret)
+    auth.set_access_token(rck.access_token, rck.access_token_secret)
+
+    return tweepy.API(auth)
 
 
+def tweet(api: tweepy.API, message: str, image_path=None):
+    api.update_status_with_media(message, image_path)
+
+    print("tweeted successfully")
+
+
+if __name__ == "__main__":
+    api = api()
+    ## tweet(api, #"this was tweeted from python"-message, #imagepath????)
